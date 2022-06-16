88ef7e74649220194121893988132e46c21a993c
Sotiris64 <hugobillemartins@gmail.com>
2022-06-15 21:45:06 -0300

    mudanças no functional, principalmente para a análise de imagens

--- functional.js
+++ functional.js
@@ -12,7 +12,6 @@ function arc(ctx, x, y, radius, start, end, color = "white", mode = "fill") {
   mode === "stroke" ? ctx.stroke() : ctx.fill();
 }
 
-// TODO clear with transform
 function clear(ctx, canvas) {
   ctx.save();
   ctx.translate(canvas.width / 2, canvas.height / 2);
@@ -36,22 +35,26 @@ function map(n, start, stop, start2, stop2) {
 function loop(i, callback) {
   function wraper(...args) {
     for (let j = 0; j < i; j++) {
-      callback(...args);
+      callback(i, ...args);
     }
   }
   return wraper;
 }
 
-function random_color(alpha=1, offset=0) {
+function random_color(alpha = 1, offset = 0) {
   return `rgba(${random(offset, 255)}, ${random(offset, 255)}, ${random(
     offset,
     255
   )}, ${alpha})`;
 }
 
-function map_color(val, s, e, s1, e1){
-  const mapped = map(val, s, e, s1, e1)
-  return `rgba(${mapped}, ${mapped}, ${mapped}, ${1})`
+function map_color(val, s, e, s1, e1) {
+  const mapped = map(val, s, e, s1, e1);
+  return `rgba(${mapped}, ${mapped}, ${mapped}, ${1})`;
+}
+
+function objColorToString(color) {
+  return `rgba(${color.r}, ${color.g}, ${color.b}, ${color.alpha})`;
 }
 
 function swap(arr, id1, id2) {
@@ -60,8 +63,48 @@ function swap(arr, id1, id2) {
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-function start(canvas, width, height, options = {}){
+function start(canvas, width, height, options = {}) {
   canvas.width = width;
-  canvas.height = height
-  return [canvas, canvas.getContext("2d", options)]
+  canvas.height = height;
+  return [canvas, canvas.getContext("2d", options)];
+}
+
+function getImageData(ctx, canvas) {
+  ctx.save();
+  ctx.resetTransform();
+  const aux = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  ctx.restore();
+  return aux;
+}
+
+function imageDataLoop(imageData, callback) {
+  for (let y = 0; y < imageData.height; y++) {
+    for (let x = 0; x < imageData.width; x++) {
+      const r = imageData.data[y * 4 * imageData.width + x * 4];
+      const g = imageData.data[y * 4 * imageData.width + (x * 4 + 1)];
+      const b = imageData.data[y * 4 * imageData.width + (x * 4 + 2)];
+      const alpha = map(
+        imageData.data[y * 4 * imageData.width + (x * 4 + 3)],
+        0,
+        255,
+        0,
+        1
+      );
+      const pixel = {
+        r,
+        g,
+        b,
+        alpha,
+      };
+
+      callback(pixel, x, y);
+    }
+  }
+}
+
+function brightnessCalc(red, green, blue) {
+  return (
+    Math.sqrt(red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114) /
+    100
+  );
 }
